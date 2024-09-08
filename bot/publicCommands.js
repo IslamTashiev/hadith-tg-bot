da718feb63ff3947b32b4532afee4a9b5e0c2e69
IslamTashiev <ueaten04@gmail.com>
2024-09-09 01:51:46 +0600

    change check_your_self command logic

--- bot/publicCommands.js
+++ bot/publicCommands.js
@@ -34,38 +34,28 @@ module.exports.handlePublicCommands = (bot, msg) => {
       const isUserAttemptsExists = userAttempts.attemptsPerDay > userAttempts.usedAttempts;
 
       if (isUserAttemptsExists) {
-        await bot.sendMessage(chatId, botTexts.chose_dificult, options.dificultLevels);
+        const currentRate = { max: 500, ratio: 0.67 };
+        const hadith = await getHadith(1500, 500);
+        const hadithText = `${hadith.title}\n\n${hadith.text}`;
 
-        bot.emit("chose_dificult", async (dificult) => {
-          const rates = {
-            1: { max: 300, ratio: 0.5 },
-            2: { max: 500, ratio: 0.67 },
-            3: { min: 700, ratio: 1.113 },
-          };
+        userContexts[chatId] = { hadith };
 
-          const currentRate = rates[dificult];
-          const hadith = await getHadith(currentRate.max, currentRate.min);
-          const hadithText = `${hadith.title}\n\n${hadith.text}`;
+        await bot.sendMessage(chatId, hadithText, options.ready);
 
-          userContexts[chatId] = { hadith };
+        bot.emit("ready_to_test", async () => {
+          await bot.sendMessage(chatId, botTexts.send_voice);
 
-          await bot.sendMessage(chatId, hadithText, options.ready);
+          bot.emit("audio_upload", async (filePath) => {
+            const { text: transcribedText } = await openaiController.transcribe(filePath);
+            const response = await openaiController.compareHadith(hadith.text, transcribedText);
+            const stats = response.content.split("/");
 
-          bot.emit("ready_to_test", async () => {
-            await bot.sendMessage(chatId, botTexts.send_voice);
+            const score = Math.round((parseInt(stats[1]) * 0.7 + parseInt(stats[0]) * 0.2) * currentRate.ratio);
+            const text = `Вы уловили суть хадиса на ${stats[0]}%, а совпадение слов составило ${stats[1]}%. Вы заработали **${score} очков**, эти очки определят вас в топе.`;
 
-            bot.emit("audio_upload", async (filePath) => {
-              const { text: transcribedText } = await openaiController.transcribe(filePath);
-              const response = await openaiController.compareHadith(hadith.text, transcribedText);
-              const stats = response.content.split("/");
-
-              const score = Math.round((parseInt(stats[1]) * 0.7 + parseInt(stats[0]) * 0.2) * currentRate.ratio);
-              const text = `Вы уловили суть хадиса на ${stats[0]}%, а совпадение слов составило ${stats[1]}%. Вы заработали **${score} очков**, эти очки определят вас в топе.`;
-
-              await UserModel.findByIdAndUpdate(user._id, { totalScore: user.totalScore + score });
-              await bot.sendMessage(chatId, text, { parse_mode: "Markdown" });
-              fs.unlinkSync(filePath);
-            });
+            await UserModel.findByIdAndUpdate(user._id, { totalScore: user.totalScore + score });
+            await bot.sendMessage(chatId, text, { parse_mode: "Markdown" });
+            fs.unlinkSync(filePath);
           });
         });
 
